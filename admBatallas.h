078ec64c2c55075b6d718a79e8f01d7f26814afa
unknown <qsnahuel@gmail.com>
2025-07-11 14:19:52 -0300

    Version final del juego

--- admBatallas.h
+++ admBatallas.h
@@ -3,9 +3,14 @@
 
 
 float getChanceGanarSegunRonda(int rondaActual);
-bool iniciarBatalla(int& rondaActual, std::vector<float>& recursosJugador, int casaElegida, int estadisticas[]);
+void iniciarBatalla(int& rondaActual, std::vector<float>& recursosJugador, int casaElegida, int estadisticas[], bool& rondaGanada);
 bool evaluarHabActiva(const std::vector<float>& recursosJugador);
 float sumarChancesSegunSoldados(int tropas);
 bool seGanoRonda(float chance_hab);
+void ejecutarPasiva(std::vector<float>& recursosJugador, int rondaActual, int casaElegida, int& tropasCaidas, int& tropasListas, int& tropasAdicionales, int estadisticas[]);
+int getComidaGastadaRonda(int cantSoldados, std::vector<float>& recursosJugador, int casaElegida);
+int getTropasListas(const std::vector<float> recursosJugador);
+bool seActivoChanceHab(float chance_hab);
+void ejecutarActiva(std::vector<float>& recursosJugador, bool& rondaGanada, int& oroAGanar, int& tropasListas);
 
 #endif // BATALLAS_H_INCLUDED
